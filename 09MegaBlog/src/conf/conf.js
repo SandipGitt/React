ed3fc98bfa75ad5e7c83a2a9490f8456a981abd5
Sandip Neupane <sandipneupane775@gmail.com>
2024-03-11 22:37:15 +0545

    created environment  and appwrite in react project

---
+++ 09MegaBlog/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId:(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId:(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId:(import.meta.env.VITE_BUCKET_ID),
+}
+
+export default conf
