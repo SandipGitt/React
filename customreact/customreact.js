2fe02d4dd6b41fe71ca77ee7cb13a15bb5e39b72
Sandip Neupane <sandipneupane775@gmail.com>
2024-06-28 15:01:22 +0545

    made a custom react

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,18 +1,25 @@
 function customRender(reactElement,container){
-    const domElement = document.createElement()
-    (reactElement.type)
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+    // container.appendChild(domElement)
+
+    const  domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
     container.appendChild(domElement)
 }
 const reactElement = {
     type: 'a',
     props : {
-        href: 'https://google.com',
+        href: 'https://facebook.com',
         target: '_blank'
     },
-    children :'Click me to visit google'
+    children :'Click me to visit facebook'
 }
 const mainContainer =document.querySelector('#root')
 
