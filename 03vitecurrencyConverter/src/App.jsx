1ca5b2564bf6ecf4b175cc91ffd903fd9b19f174
Sandip Neupane <sandipneupane775@gmail.com>
2024-07-01 18:49:43 +0545

    Minor  changes

--- 03vitecurrencyConverter/src/App.jsx
+++ 03vitecurrencyConverter/src/App.jsx
@@ -38,7 +38,6 @@ function App() {
                     onSubmit={(e) => {
                         e.preventDefault();
                         convert()
-                       
                     }}
                 >
                     <div className="w-full mb-1">
@@ -46,7 +45,7 @@ function App() {
                             label="From"
                             amount={amount}
                             currencyOptions={options}
-                            onCurrencyChange={(currency) => setAmount(amount)}
+                            onCurrencyChange={(currency) => setFrom(currency)}
                             selectCurrency={from}
                             onAmountChange={(amount) => setAmount(amount)}
                         />
@@ -54,7 +53,7 @@ function App() {
                     <div className="relative w-full h-0.5">
                         <button
                             type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-700 text-white px-5 py-1"
                             onClick={swap}
                         >
                             swap
@@ -66,11 +65,11 @@ function App() {
                             amount={convertedAmount}
                             currencyOptions={options}
                             onCurrencyChange={(currency) => setTo(currency)}
-                            selectCurrency={from}
+                            selectCurrency={to}
                             amountDisable
                         />
                     </div>
-                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                    <button type="submit" className="w-full bg-blue-700 text-white px-4 py-4 rounded-lg">
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
                 </form>
