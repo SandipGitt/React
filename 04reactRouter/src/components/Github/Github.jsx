fd9b13464c941e4e7bcaf9046a12020633d1880d
Sandip Neupane <sandipneupane775@gmail.com>
2024-07-04 23:54:12 +0545

    Minor Update

--- 04reactRouter/src/components/Github/Github.jsx
+++ 04reactRouter/src/components/Github/Github.jsx
@@ -14,7 +14,9 @@ function Github() {
     // }, [])
     return (
     <div className='text-center m-5 bg-orange-600 text-white p-3 text-2xl'>
-        Github followers:{data.followers}</div>
+        Github followers:{data.followers}
+        <img src={data.avatar_url} alt='Github Profile' width={300}/>
+        </div>
     )
 }
 
