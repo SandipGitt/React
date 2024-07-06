8eddaa42c57adac1b11a36a60def9d2b1d4639ec
Sandip Neupane <sandipneupane775@gmail.com>
2024-07-06 22:55:55 +0545

    Updates

--- 05minicontext/src/App.jsx
+++ 05minicontext/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import UserContextProvider from './context/UserContextProvider'
 import Profile from './components/Profile'
 import './App.css'
 import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <UserContextProvider>
